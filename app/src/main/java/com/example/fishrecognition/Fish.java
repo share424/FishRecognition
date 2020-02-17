232c4fd7b7278abdae0bff52457968899b94b136
Surya <share424@gmail.com>
2020-02-17 22:30:04 +0800

    add tensorflow mobile add mobilenet model now this app can classify some fish

---
+++ app/src/main/java/com/example/fishrecognition/Fish.java
@@ -0,0 +1,37 @@
+package com.example.fishrecognition;
+
+public class Fish {
+    private String name;
+    private int price;
+    private String description;
+
+    public Fish(String name, int price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- app/src/main/java/com/example/fishrecognition/MainActivity.java
+++ app/src/main/java/com/example/fishrecognition/MainActivity.java
@@ -4,9 +4,14 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -23,24 +28,84 @@ import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 import java.text.NumberFormat;
+import java.util.ArrayList;
 import java.util.Currency;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.Interpreter;
+import org.tensorflow.lite.support.common.FileUtil;
+import org.tensorflow.lite.support.common.TensorOperator;
+import org.tensorflow.lite.support.common.TensorProcessor;
+import org.tensorflow.lite.support.common.ops.NormalizeOp;
+import org.tensorflow.lite.support.image.ImageProcessor;
+import org.tensorflow.lite.support.image.TensorImage;
+import org.tensorflow.lite.support.image.ops.ResizeOp;
+import org.tensorflow.lite.support.image.ops.ResizeWithCropOrPadOp;
+import org.tensorflow.lite.support.image.ops.Rot90Op;
+import org.tensorflow.lite.support.label.TensorLabel;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+import org.tensorflow.lite.support.tensorbuffer.TensorBufferFloat;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static final String MODEL_PATH = "model_fish.tflite";
+    public static final String LABEL_PATH = "labels.txt";
 
     @BindView(R.id.display_image_iv) ImageView imageView;
     @BindView(R.id.condition_tv) TextView condition;
     @BindView(R.id.valid_time_tv) TextView validTime;
     @BindView(R.id.price) TextView price;
     @BindView(R.id.description) TextView description;
+    @BindView(R.id.confident) TextView confident;
+    @BindView(R.id.fish) TextView fish;
 
     Random rand = new Random();
+    private MappedByteBuffer tfliteModel;
+    private Interpreter tflite;
+    private final Interpreter.Options tfliteOptions = new Interpreter.Options();
+    private List<String> labels;
+
+    /** Image size along the x axis. */
+    private int imageSizeX;
+
+    /** Image size along the y axis. */
+    private int imageSizeY;
+
+    /** Input image TensorBuffer. */
+    private TensorImage inputImageBuffer;
+
+    /** Output probability TensorBuffer. */
+    private TensorBuffer outputProbabilityBuffer;
+
+    /** Processer to apply post processing of the output probability. */
+    private TensorProcessor probabilityProcessor;
+
+    /**
+     * The quantized model does not require normalization, thus set mean as 0.0f, and std as 1.0f to
+     * bypass the normalization.
+     */
+    private static final float IMAGE_MEAN = 127.5f;
+
+    private static final float IMAGE_STD = 127.5f;
+
+    /** Quantized MobileNet requires additional dequantization to the output probability. */
+    private static final float PROBABILITY_MEAN = 0.0f;
+
+    private static final float PROBABILITY_STD = 1.0f;
+
+    private List<Fish> fishList = new ArrayList<>();
 
     @OnClick(R.id.take_photo_btn) void onTakePhoto() {
         //check permission dulu coy
@@ -49,6 +114,7 @@ public class MainActivity extends AppCompatActivity {
                 .withListener(new PermissionListener() {
                     @Override public void onPermissionGranted(PermissionGrantedResponse response) {
                         ImagePicker.cameraOnly().start(MainActivity.this);
+//                        ImagePicker.create(MainActivity.this).start();
                     }
                     @Override public void onPermissionDenied(PermissionDeniedResponse response) {
                         Toast.makeText(MainActivity.this, "Please grant camera permission", Toast.LENGTH_LONG).show();
@@ -64,23 +130,91 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        initializeClassifier();
+        initializeFish();
+    }
 
+    private void initializeFish() {
+        Fish tongkol = new Fish("Tongkol", 30000, "Protein, Kalium, Magnesium, VIT A, dan VIT E");
+        Fish gurame = new Fish("Gurame", 60000, "Protein Tinggi, VIT E, dan Kolestrol Rendah");
+        Fish kakap_merah = new Fish("Kakap Merah", 50000, "Kalsium, VIT A, VIT D, VIT E, dan Protein");
+        Fish nila = new Fish("Nila", 25000, "Protein Tinggi, Omega 3, Fosfor, VIT B12, VIT B5, dan Antioksidan");
+        Fish mujair = new Fish("Mujair", 35000, "Protein, Asam Lemak Omega 3, Asam Lemak Omegra 6, dan Kalsium");
+        fishList.add(kakap_merah);
+        fishList.add(gurame);
+        fishList.add(mujair);
+        fishList.add(nila);
+        fishList.add(tongkol);
+    }
+
+    private void initializeClassifier() {
+        try {
+            tfliteModel = FileUtil.loadMappedFile(this, MODEL_PATH);
+            tflite = new Interpreter(tfliteModel);
+            labels = FileUtil.loadLabels(this, LABEL_PATH);
+
+            // Reads type and shape of input and output tensors, respectively.
+            int imageTensorIndex = 0;
+            int[] imageShape = tflite.getInputTensor(imageTensorIndex).shape(); // {1, height, width, 3}
+            imageSizeY = imageShape[1];
+            imageSizeX = imageShape[2];
+            DataType imageDataType = tflite.getInputTensor(imageTensorIndex).dataType();
+            int probabilityTensorIndex = 0;
+            int[] probabilityShape =
+                    tflite.getOutputTensor(probabilityTensorIndex).shape(); // {1, NUM_CLASSES}
+            DataType probabilityDataType = tflite.getOutputTensor(probabilityTensorIndex).dataType();
+
+            // Creates the input tensor.
+            inputImageBuffer = new TensorImage(DataType.FLOAT32);
+
+            // Creates the output tensor and its processor.
+            outputProbabilityBuffer = TensorBuffer.createFixedSize(probabilityShape, probabilityDataType);
+
+            // Creates the post processor for the output probability.
+            probabilityProcessor = new TensorProcessor.Builder().add(getPostprocessNormalizeOp()).build();
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         if (ImagePicker.shouldHandle(requestCode, resultCode, data)) {
-            // Get a list of picked images
-            List<Image> images = ImagePicker.getImages(data);
-            // or get a single image only
+            // get a single image only
             Image image = ImagePicker.getFirstImageOrNull(data);
             Glide.with(this).load(image.getPath()).into(imageView);
+            // for now generate random fish condition
             boolean cond = generateCondition();
             condition.setText(cond ? "Segar" : "Tidak Segar");
+            // and generate random fish expired time
             int valid = generateValidateTime(cond);
             validTime.setText(valid + " Hari");
-            price.setText(generatePrice(cond, valid));
-            description.setText(generateDescription(cond));
+            // let's get the fish bitmap image
+            Bitmap bmp = BitmapFactory.decodeFile(image.getPath());
+            // and predict it
+            Map<String, Float> probabilities = recognizeImage(bmp);
+            float best = 0;
+            String predictedLabel = labels.get(0);
+            int idx = 0;
+            int predictedIndex = 0;
+            // find the best probability
+            for(Map.Entry<String, Float> entry : probabilities.entrySet()) {
+                if(entry.getValue() > best) {
+                    best = entry.getValue();
+                    // well because we use fish object, we didn't need the label anymore
+                    predictedLabel = entry.getKey();
+                    predictedIndex = idx;
+                }
+                idx++;
+            }
+            // set the labels on the screen
+            fish.setText(fishList.get(predictedIndex).getName());
+            price.setText(generatePrice(fishList.get(predictedIndex).getPrice()));
+            confident.setText(String.format("%.2f",best*100)+"%");
+            description.setText(fishList.get(predictedIndex).getDescription());
+
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
@@ -91,28 +225,56 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public int generateValidateTime(boolean condition) {
-        int conditionLenght = 1 + rand.nextInt(condition ? 7 : 3);
-        return conditionLenght;
+        return 1 + rand.nextInt(condition ? 7 : 3);
     }
 
-    public String generatePrice(boolean condition, int valid) {
-        int startPrice = 20;
-        if(condition) {
-            valid *= 2;
-            startPrice += valid;
-        }
-        int price = (startPrice + rand.nextInt(startPrice * 2)) * 500;
+    public String generatePrice(int price) {
         NumberFormat format = NumberFormat.getCurrencyInstance();
         format.setMaximumFractionDigits(0);
         format.setCurrency(Currency.getInstance("IDR"));
         return format.format(price);
     }
 
-    public String generateDescription(boolean condition) {
-        if(condition) {
-            return "Ikan segar yang memiliki banyak manfaat";
-        } else {
-            return "Ikan tidak segar yang kurang bermanfaat";
-        }
+    /** Loads input image, and applies preprocessing. */
+    private TensorImage loadImage(final Bitmap bitmap) {
+        // Loads bitmap into a TensorImage.
+        inputImageBuffer.load(bitmap);
+
+        // Creates processor for the TensorImage.
+        int cropSize = Math.min(bitmap.getWidth(), bitmap.getHeight());
+        // the screen always in portrait
+        int numRoration = 0 / 90;
+        // TODO(b/143564309): Fuse ops inside ImageProcessor.
+        ImageProcessor imageProcessor =
+                new ImageProcessor.Builder()
+                        .add(new ResizeWithCropOrPadOp(cropSize, cropSize))
+                        .add(new ResizeOp(imageSizeX, imageSizeY, ResizeOp.ResizeMethod.NEAREST_NEIGHBOR))
+                        .add(new Rot90Op(numRoration))
+                        .add(getPreprocessNormalizeOp())
+                        .build();
+        return imageProcessor.process(inputImageBuffer);
     }
+
+    protected TensorOperator getPreprocessNormalizeOp() {
+        return new NormalizeOp(IMAGE_MEAN, IMAGE_STD);
+    }
+
+    protected TensorOperator getPostprocessNormalizeOp() {
+        return new NormalizeOp(PROBABILITY_MEAN, PROBABILITY_STD);
+    }
+
+    /** Runs inference and returns the classification results. */
+    public Map<String, Float> recognizeImage(final Bitmap bitmap) {
+        inputImageBuffer = loadImage(bitmap);
+
+
+        tflite.run(inputImageBuffer.getBuffer(), outputProbabilityBuffer.getBuffer().rewind());
+
+        // Gets the map of label and probability.
+        Map<String, Float> labeledProbability =
+                new TensorLabel(labels, probabilityProcessor.process(outputProbabilityBuffer))
+                        .getMapWithFloatValue();
+        return labeledProbability;
+    }
+
 }
